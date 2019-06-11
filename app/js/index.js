81378a0fda02ec245d8be6b0a6f49cbbfbe0193e
Janwibool <Sanhanut.Janwibool@thomsonreuters.com>
2019-06-11 15:59:43 +0700

    send request to mock server

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,16 @@
 const { ipcRenderer } = require('electron')
+const axios = require('axios')
 
 document.getElementById('send').addEventListener("click", () => {
     ipcRenderer.send('main-window-message', document.getElementById('input').value)
 })
+
+document.getElementById('get').onclick = () => {
+    axios({
+        method: 'get',
+        url: 'http://localhost:3000/data'
+    })
+        .then(function (response) {
+            console.log(response)
+        });
+}
